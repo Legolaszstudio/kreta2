1b8b576b85645d5fe8f07ed0186d5ea61279aafd
huncut2016 <56272960+huncut2016@users.noreply.github.com>
2019-11-15 23:32:26 +0100

    Update kreta.js

--- kreta.js
+++ kreta.js
@@ -149,7 +149,29 @@ switch (Math.round(atlag)) {
                     
                     break;
                case 2:
-                    alert("szerezz"+" "+th+"db"+" "+"kettest")  //ha csak 2 kell)
+                     if (x-5*th == 0) {
+                        alert("szerezz"+" "+th+"db"+" "+"ötöst") //ha csak 5 kell
+                    }else{
+                        if(x-5*th > 0){
+                            alert("nem lehet megcsinálni")       //ha nem lehet megcsinálni 
+                        }else{
+                           if(Math.abs(x-5*th) > 5){
+                               if (Math.abs(x-4*th) > 5) {
+                                   if (Math.abs(x-3*th) > 5) {
+                                       if (Math.abs(x-2*th) > 5) {
+                                           alert("Szerezz"+" "+Math.round((th-Math.abs(x-2*th)))+"db"+" "+"kettest"+ " "+"és"+" "+Math.round((Math.abs(x-2*th))) +"db"+" "+"egyest") // 2 és 1 kell
+                                       }
+                                   }else{
+                                       alert("Szerezz"+" "+Math.round((th-Math.abs(x-3*th)))+"db"+" "+"hármast"+ " "+"és"+" "+Math.round((Math.abs(x-3*th))) +"db"+" "+"kettest")  // 3 és vagy 2 kell 
+                                   }
+                               }else{
+                                   alert("Szerezz"+" "+Math.round((th-Math.abs(x-4*th)))+"db"+" "+"négyest"+ " "+"és"+" "+Math.round((Math.abs(x-4*th))) +"db"+" "+"hármast") //4 és vagy 3 kell
+                               }
+                           }else{
+                               alert("Szerezz"+" "+Math.round((th-Math.abs(x-5*th)))+"db"+" "+"ötöst"+ " "+"és"+" "+Math.round((Math.abs(x-5*th))) +"db"+" "+"négyest") //5 és vagy 4 kell
+                           }
+                        }
+                    }
                    break;
                case 3:
                    if (x-5*th == 0) {
@@ -305,39 +327,29 @@ switch (Math.round(atlag)) {
                            }
                    break;
                case 3:
-                       while(j1>elak){
-   
-                           j2=j2-1;
-                           j1= (jegyek+j2)/(th+jsz);
-                           }
-                           if (j2-th<0) {
-                             alert("nem lehet megcsinálni")
-                           } else {
-                             if (j2-th==0) {
-                               alert(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
-                             }else{
-                               if (j2-5*th>0) {
-                                 alert("nem lehet megcsinálni");
-                               } else {
-                                 if (j2-th > 5) {
-                                   if (j2-th*2 > 5) {
-                                     if (j2-th*3 > 5) {
-                                       if (j2-th*4 > 5) {
-                                         alert(Math.round(th-(j2-4*th))+"db"+" "+"négyes"+" "+"és"+" "+Math.round(j2-4*th)+" "+"ötös");// 4 5 
+                      if (x-5*th == 0) {
+                        alert("szerezz"+" "+th+"db"+" "+"ötöst") //ha csak 5 kell
+                    }else{
+                        if(x-5*th > 0){
+                            alert("nem lehet megcsinálni")       //ha nem lehet megcsinálni 
+                        }else{
+                           if(Math.abs(x-5*th) > 5){
+                               if (Math.abs(x-4*th) > 5) {
+                                   if (Math.abs(x-3*th) > 5) {
+                                       if (Math.abs(x-2*th) > 5) {
+                                           alert("Szerezz"+" "+Math.round((th-Math.abs(x-2*th)))+"db"+" "+"kettest"+ " "+"és"+" "+Math.round((Math.abs(x-2*th))) +"db"+" "+"egyest") // 2 és 1 kell
                                        }
-                                         
-                                       } else {
-                                       alert(Math.round(th-(j2-3*th))+"db"+" "+"hármas"+" "+"és"+" "+Math.round(j2-3*th)+" "+"négyes");// 3 4
-                                     }
-                                   } else {
-                                     alert(Math.round(th-(j2-2*th))+"db"+" "+"kettes"+" "+"és"+" "+Math.round(j2-2*th)+" "+"hármas");//2 3 
+                                   }else{
+                                       alert("Szerezz"+" "+Math.round((th-Math.abs(x-3*th)))+"db"+" "+"hármast"+ " "+"és"+" "+Math.round((Math.abs(x-3*th))) +"db"+" "+"kettest")  // 3 és vagy 2 kell 
                                    }
-                                 } else {
-                                   alert(Math.round(th-(j2-th))+"db"+" "+"egyes"+" "+"és"+" "+Math.round(j2-th)+" "+"kettes"); //1 2 
-                                 }
+                               }else{
+                                   alert("Szerezz"+" "+Math.round((th-Math.abs(x-4*th)))+"db"+" "+"négyest"+ " "+"és"+" "+Math.round((Math.abs(x-4*th))) +"db"+" "+"hármast") //4 és vagy 3 kell
                                }
-                             }
+                           }else{
+                               alert("Szerezz"+" "+Math.round((th-Math.abs(x-5*th)))+"db"+" "+"ötöst"+ " "+"és"+" "+Math.round((Math.abs(x-5*th))) +"db"+" "+"négyest") //5 és vagy 4 kell
                            }
+                        }
+                    }
                    break;
                case 4:
                        if (x-5*th == 0) {
@@ -399,7 +411,7 @@ switch (Math.round(atlag)) {
                           alert("nem lehet megcsinálni")
                         } else {
                           if (j2-th==0) {
-                            console.log(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
+                            alert(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
                           }else{
                             if (j2-5*th>0) {
                               alert("nem lehet megcsinálni");
@@ -434,7 +446,7 @@ switch (Math.round(atlag)) {
                           alert("nem lehet megcsinálni")
                         } else {
                           if (j2-th==0) {
-                            console.log(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
+                            alert(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
                           }else{
                             if (j2-5*th>0) {
                               alert("nem lehet megcsinálni");
@@ -469,7 +481,7 @@ switch (Math.round(atlag)) {
                           alert("nem lehet megcsinálni")
                         } else {
                           if (j2-th==0) {
-                            console.log(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
+                            alert(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
                           }else{
                             if (j2-5*th>0) {
                               alert("nem lehet megcsinálni");
@@ -495,7 +507,29 @@ switch (Math.round(atlag)) {
                         } 
                 break;
             case 4:
-                        alert(Math.round(th)+"db"+" "+"négyes kell")
+                         if (x-5*th == 0) {
+                        alert("szerezz"+" "+th+"db"+" "+"ötöst") //ha csak 5 kell
+                    }else{
+                        if(x-5*th > 0){
+                            alert("nem lehet megcsinálni")       //ha nem lehet megcsinálni 
+                        }else{
+                           if(Math.abs(x-5*th) > 5){
+                               if (Math.abs(x-4*th) > 5) {
+                                   if (Math.abs(x-3*th) > 5) {
+                                       if (Math.abs(x-2*th) > 5) {
+                                           alert("Szerezz"+" "+Math.round((th-Math.abs(x-2*th)))+"db"+" "+"kettest"+ " "+"és"+" "+Math.round((Math.abs(x-2*th))) +"db"+" "+"egyest") // 2 és 1 kell
+                                       }
+                                   }else{
+                                       alert("Szerezz"+" "+Math.round((th-Math.abs(x-3*th)))+"db"+" "+"hármast"+ " "+"és"+" "+Math.round((Math.abs(x-3*th))) +"db"+" "+"kettest")  // 3 és vagy 2 kell 
+                                   }
+                               }else{
+                                   alert("Szerezz"+" "+Math.round((th-Math.abs(x-4*th)))+"db"+" "+"négyest"+ " "+"és"+" "+Math.round((Math.abs(x-4*th))) +"db"+" "+"hármast") //4 és vagy 3 kell
+                               }
+                           }else{
+                               alert("Szerezz"+" "+Math.round((th-Math.abs(x-5*th)))+"db"+" "+"ötöst"+ " "+"és"+" "+Math.round((Math.abs(x-5*th))) +"db"+" "+"négyest") //5 és vagy 4 kell
+                           }
+                        }
+                    }
                 break
             case 5:
                     if (x-5*th == 0) {
@@ -537,7 +571,7 @@ switch (Math.round(atlag)) {
                                  alert("nem lehet megcsinálni")
                                } else {
                                  if (j2-th==0) {
-                                   console.log(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
+                                   alert(Math.round(th)+"db"+" "+"egyes"+" "+"kell")
                                  }else{
                                    if (j2-5*th>0) {
                                      alert("nem lehet megcsinálni");
